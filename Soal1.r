92f41fb6e30c068e7faae0a439e45773e8120620
masnurrm <nurmuhammad448@gmail.com>
2022-05-29 09:46:39 +0700

    feat: add soal3

--- Soal1.r
+++ Soal1.r
@@ -1,21 +1,7 @@
 # Poin A
-p = 0.2
-x = 3
-P <- dgeom(x, p)
-P
+
 
 # Poin B
-mean(rgeom(n = 10000, prob = p) == 3)
+
 
 # Poin C
-# Penjelasan terdapat pada writeup dokumentasi readme.md
-
-# Poin D
-n = 10000
-hist(rgeom(n, prob = p), main = 'Histogram Geometrik')
-
-# Poin E
-mean = 1 / p
-variance = (1 - p) / (p ^ 2)
-mean
-variance
--- Soal4.r
+++ Soal4.r
@@ -1,16 +1,17 @@
-x = 2
-v = 10
-
 # Poin A
-probability = dchisq(x, 10)
-probability
+
 
 # Poin B
-n = 100
-hist(rchisq(n, v), xlab = "X", ylab = "V", main = "Grafik Histogram")
+
 
 # Poin C
-mean = v
-variance = 2 * v
-mean
-variance
+
+
+# Poin D
+
+
+# Poin E
+
+
+# Poin F
+
